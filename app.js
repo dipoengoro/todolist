9ce141b269fd7c3ed317c54f3655a8d362860272
dipoengoro <dipoengoro@outlook.com>
2022-01-24 15:24:40 +0700

    First Commit

--- app.js
+++ app.js
@@ -4,10 +4,32 @@ const bodyParser = require('body-parser');
 const app = express();
 const PORT = 3000;
 
+app.use(express.static("public"))
+app.use(bodyParser.urlencoded({ extended: true }))
+
 app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
-  res.send("It is working")
+  var date = new Date()
+  var currentDay = date.getDay()
+  // var currentDay = 6
+
+  var options = {
+    weekday: 'long',
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric'
+  }
+
+  var day = date.toLocaleDateString('id-ID', options)
+  res.render('list', {day: day})
+})
+
+app.post('/', (req, res) => {
+  var item = req.body.newItem
+  var items = []
+  items.push(item)
+  res.send('Received')
 })
 
 app.listen(PORT, () => {
